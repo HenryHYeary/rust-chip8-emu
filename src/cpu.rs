76e0ac459f241b56de202d035ced2e5d5dc16cd5
HenryHYeary <henmofb@yahoo.com>
2026-06-14 18:34:50 -0500

    Finish CPU implementation, add rand package.

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 pub const RAM_SIZE: usize = 4096;
 pub const PROGRAM_START: u16 = 0x200;
 
@@ -110,7 +112,7 @@ impl Cpu {
                 0x5 => self.op_sub(x, y),
                 0x6 => self.op_shr(x),
                 0x7 => self.op_subn(x, y),
-                0xE => self.op_shl(x, y),
+                0xE => self.op_shl(x),
                 _ => eprintln!("Unknown opcode: {:#06X}", op),                
             },
 
@@ -120,7 +122,228 @@ impl Cpu {
             0xC => self.op_rnd(x, nn),
             0xD => self.op_drw(x, y, n),
 
-            
+            0xE => match nn {
+                0x9E => self.op_skp(x),
+                0xA1 => self.op_sknp(x),
+                _ => eprintln!("Unknown opcode: {:06X}", op),
+            },
+
+            0xF => match nn {
+                0x07 => self.op_ld_vx_dt(x),
+                0x0A => self.op_ld_vx_k(x),
+                0x15 => self.op_ld_dt_vx(x),
+                0x18 => self.op_ld_st_vx(x),
+                0x1E => self.op_add_i_vx(x),
+                0x29 => self.op_ld_f_vx(x),
+                0x33 => self.op_ld_b_vx(x),
+                0x55 => self.op_ld_i_vx(x),
+                0x65 => self.op_ld_vx_i(x),
+                _ => eprintln!("Unknown opcode: {:06X}", op),
+            },
+
+            _ => eprintln!("Unknown opcode: {:06X}", op),
+        }
+    }
+
+    fn op_cls(&mut self) {
+        self.display = [false; DISPLAY_WIDTH * DISPLAY_HEIGHT];
+        self.draw_flag = true;
+    }
+
+    fn op_ret(&mut self) {
+        self.sp -= 1;
+        self.pc = self.stack[self.sp];
+    }
+
+    fn op_jp(&mut self, nnn: u16) {
+        self.pc = nnn;
+    }
+
+    fn op_call(&mut self, nnn: u16) {
+        self.stack[self.sp] = self.pc;
+        self.sp += 1;
+        self.pc = nnn;
+    }
+
+    fn op_se_vx_byte(&mut self, x: usize, nn: u8) {
+        if self.v[x] == nn {
+            self.pc += 2;
+        }
+    }
+
+    fn op_sne_vx_byte(&mut self, x: usize, nn: u8) {
+        if self.v[x] != nn {
+            self.pc += 2;
+        }
+    }
+
+    fn op_se_vx_vy(&mut self, x: usize, y: usize) {
+        if self.v[x] == self.v[y] {
+            self.pc += 2;
+        }
+    }
+
+    fn op_ld_vx_byte(&mut self, x: usize, nn: u8) {
+        self.v[x] = nn;
+    }
+
+    fn op_add_vx_byte(&mut self, x: usize, nn: u8) {
+        self.v[x] = self.v[x].wrapping_add(nn);
+    }
+
+    fn op_ld_vx_vy(&mut self, x: usize, y: usize) {
+        self.v[x] = self.v[y];
+    }
+
+    fn op_or(&mut self, x: usize, y: usize) {
+        self.v[x] |= self.v[y];
+    }
+
+    fn op_and(&mut self, x: usize, y: usize) {
+        self.v[x] &= self.v[y];
+    }
+
+    fn op_xor(&mut self, x: usize, y: usize) {
+        self.v[x] ^= self.v[y];
+    }
+
+    fn op_add_vx_vy(&mut self, x: usize, y: usize) {
+        let (result, overflow) = self.v[x].overflowing_add(self.v[y]);
+        self.v[x] = result;
+        self.v[0xF] = overflow as u8;
+    }
+
+    fn op_sub(&mut self, x: usize, y: usize) {
+        let (result, borrow) = self.v[x].overflowing_sub(self.v[y]);
+        self.v[x] = result;
+        self.v[0xF] = !borrow as u8;
+    }
+
+    fn op_shr(&mut self, x: usize) {
+        self.v[0xF] = self.v[x] & 0x1;
+        self.v[x] >>= 1;
+    }
+
+    fn op_subn(&mut self, x: usize, y: usize) {
+        let (result, borrow) = self.v[y].overflowing_sub(self.v[x]);
+        self.v[x] = result;
+        self.v[0xF] = !borrow as u8;
+    }
+
+    fn op_shl(&mut self, x: usize) {
+        self.v[0xF] = (self.v[x] >> 7) & 0x1;
+        self.v[x] <<= 1;
+    }
+
+    fn op_sne_vx_vy(&mut self, x: usize, y: usize) {
+        if self.v[x] != self.v[y] {
+            self.pc += 2;
+        }
+    }
+
+    fn op_ld_i(&mut self, nnn: u16) {
+        self.i = nnn;
+    }
+
+    fn op_jp_v0(&mut self, nnn: u16) {
+        self.pc = nnn + self.v[0] as u16;
+    }
+
+    fn op_rnd(&mut self, x: usize, nn: u8) {
+        let rand_byte = rand::thread_rng().gen::<u8>() & nn;
+        self.v[x] = rand_byte & nn;
+    }
+
+    fn op_drw(&mut self, x: usize, y: usize, n: u8) {
+        let x_start = self.v[x] as usize % DISPLAY_WIDTH;
+        let y_start = self.v[y] as usize % DISPLAY_HEIGHT;
+        self.v[0xF] = 0;
+
+        for row in 0..n as usize {
+            let sprite_byte  = self.ram[self.i as usize + row];
+            let py = (y_start + row) % DISPLAY_HEIGHT;
+
+            for col in 0..8usize {
+                if sprite_byte & (0x80 >> col) != 0 {
+                    let px = (x_start + col) % DISPLAY_WIDTH;
+                    let idx = py * DISPLAY_WIDTH + px;
+
+                    if self.display[idx] {
+                        self.v[0xF] = 1;
+                    }
+                    self.display[idx] ^= true;
+                }
+            }
+        }
+
+        self.draw_flag = true;
+    }
+
+    fn op_skp(&mut self, x: usize) {
+        if self.keys[self.v[x] as usize] {
+            self.pc += 2;
+        }
+    }
+
+    fn op_sknp(&mut self, x: usize) {
+        if !self.keys[self.v[x] as usize] {
+            self.pc += 2;
+        }
+    }
+
+    fn op_ld_vx_dt(&mut self, x: usize) {
+        self.v[x] = self.delay_timer;
+    }
+
+    fn op_ld_vx_k(&mut self, x: usize) {
+        if let Some(key) = self.keys.iter().position(|&k| k) {
+            self.v[x] = key as u8;
+        } else {
+            self.pc -= 2;
+        }
+    }
+
+    fn op_ld_dt_vx(&mut self, x: usize) {
+        self.delay_timer = self.v[x];
+    }
+
+    fn op_ld_st_vx(&mut self, x: usize) {
+        self.sound_timer = self.v[x];
+    }
+
+    fn op_add_i_vx(&mut self, x: usize) {
+        self.i = self.i.wrapping_add(self.v[x] as u16);
+    }
+
+    fn op_ld_f_vx(&mut self, x: usize) {
+        self.i = (FONT_START + (self.v[x] as usize & 0xF) * 5) as u16;
+    }
+
+    fn op_ld_b_vx(&mut self, x: usize) {
+        let val = self.v[x];
+        self.ram[self.i as usize] = val / 100;
+        self.ram[self.i as usize + 1] = (val / 10) % 10;
+        self.ram[self.i as usize + 2] = val % 10;
+    }
+
+    fn op_ld_i_vx(&mut self, x: usize) {
+        for reg in 0..=x {
+            self.ram[self.i as usize + reg] = self.v[reg];
+        }
+    }
+
+    fn op_ld_vx_i(&mut self, x: usize) {
+        for reg in 0..=x {
+            self.v[reg] = self.ram[self.i as usize + reg];
+        }
+    }
+
+    pub fn tick_timers(&mut self) {
+        if self.delay_timer > 0 {
+            self.delay_timer -= 1;
+        }
+        if self.sound_timer > 0 {
+            self.sound_timer -= 1;
         }
     }
 }
